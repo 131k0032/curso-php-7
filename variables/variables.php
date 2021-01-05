854da6c9cd4bc9a603dda08216fcf41124d7ef8d
Trainer <mitnick117@gmail.com>
2021-01-05 13:50:17 -0500

    Primer commit, tipos de variables en PHP 7

---
+++ variables/variables.php
@@ -0,0 +1,25 @@
+<?php
+
+//Variables String
+$nombre="Pablo";
+$apellido="Sanchez";
+$mensaje_saludo = "Hola estamos todos bien";
+
+//Variables enteros
+$edad =30;
+
+//Variables flotantes
+$peso=70.6;
+
+//Variables boleanos
+$encendido= true; //Boleano
+
+//variables null o nulos
+$nombre =null;
+
+//Mostramos el tipo de variables
+echo gettype($peso);
+
+
+
+ ?>
