95f62e43b512a8b50e8acf9397a585f5a27090aa
Trainer <mitnick117@gmail.com>
2021-01-05 15:44:27 -0500

    Archivo de concatenaciones.php

---
+++ concatenaciones/concatenaciones.php
@@ -0,0 +1,18 @@
+<?php
+
+$saludo="Hola mundo";
+$mensaje="El ganador es el numero ";
+$ganador=8889;
+
+//se puede mezclar mientras esté dentro de comillas dobles
+echo "el numero ganador es $ganador";
+
+//concantenando con punto
+echo "el numero ganador es" . $ganador;
+
+//concantenando con commillas simples
+echo 'el numero ganador es' .$ganador;
+
+echo "yo le dije \"hola mundo\" ";
+
+ ?>
